f5a7ae97ed2f4410d9eefa20b2c020aef05435d2
Eric Rasche <rasche.eric@gmail.com>
2016-03-08 20:57:40 +0000

    Refactor math

--- js/Store/SeqFeature/GCContent.js
+++ js/Store/SeqFeature/GCContent.js
@@ -28,20 +28,21 @@ return declare( SeqFeatureStore, {
     },
 
     getFeatures: function( query, featureCallback, finishCallback, errorCallback ) {
-        query.start = Math.max( 0, query.start - this.windowSize/2 );
-        query.end = Math.min( query.end + this.windowSize/2, this.browser.refSeq.length );
+        this.halfWindow = this.windowSize/2;
+        query.start = Math.max( 0, query.start - this.halfWindow );
+        query.end = Math.min( query.end + this.halfWindow, this.browser.refSeq.length );
         var thisB = this;
 
         if( query.end < 0 || query.start > query.end ) {
             finishCallback();
             return;
         }
-        
+
         this.store.getReferenceSequence(
             query,
             function( residues ) {
-                for( var i = thisB.windowSize/2; i < residues.length - thisB.windowSize/2; i+=thisB.windowDelta ) {
-                    var r = residues.slice( i - thisB.windowSize/2, i + thisB.windowSize/2 );
+                for( var i = thisB.halfWindow; i < residues.length - thisB.halfWindow; i+=thisB.windowDelta ) {
+                    var r = residues.slice( i - thisB.halfWindow, i + thisB.halfWindow );
                     var n = 0;
                     for( var j = 0; j < r.length; j++ ) {
                         if(r[j]=="c"||r[j]=="g"||r[j]=="G"||r[j]=="C") n++;
