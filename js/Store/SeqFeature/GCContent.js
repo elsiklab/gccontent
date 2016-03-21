24c0c7b34dd157aea6955a9bf35375491de8c5c6
Colin <colin.diesh@gmail.com>
2016-03-21 16:19:29 -0500

    Use halfWindow as a local variable

--- js/Store/SeqFeature/GCContent.js
+++ js/Store/SeqFeature/GCContent.js
@@ -29,9 +29,9 @@ return declare( SeqFeatureStore, {
     },
 
     getFeatures: function( query, featureCallback, finishCallback, errorCallback ) {
-        this.halfWindow = this.windowSize/2;
-        query.start = Math.max( 0, query.start - this.halfWindow );
-        query.end = Math.min( query.end + this.halfWindow, this.browser.refSeq.length );
+        var hw = this.windowSize/2; // half the window size
+        query.start = Math.max( 0, query.start - hw );
+        query.end = Math.min( query.end + hw, this.browser.refSeq.length );
         var thisB = this;
 
         if( query.end < 0 || query.start > query.end ) {
@@ -42,19 +42,20 @@ return declare( SeqFeatureStore, {
         this.store.getReferenceSequence(
             query,
             function( residues ) {
-                for( var i = thisB.halfWindow; i < residues.length - thisB.halfWindow; i+=thisB.windowDelta ) {
-                    var r = residues.slice( i - thisB.halfWindow, i + thisB.halfWindow );
+                for( var i = hw; i < residues.length - hw; i += thisB.windowDelta ) {
+                    var r = residues.slice( i - hw, i + hw );
                     var nc = 0;
                     var ng = 0;
                     for( var j = 0; j < r.length; j++ ) {
-                        if(r[j]=="c"||r[j]=="C") nc++;
-                        else if(r[j]=="g"||r[j]=="G") ng++;
+                        if(r[j] == "c" || r[j] == "C") nc++;
+                        else if(r[j] == "g" || r[j] == "G") ng++;
                     }
                     var pos = query.start;
                     var score;
                     if(thisB.gcMode === 'content') {
-                        score = (ng + nc)/r.length
-                    }else if(thisB.gcMode === 'skew') {
+                        score = (ng + nc) / r.length
+                    }
+                    else if(thisB.gcMode === 'skew') {
                         score = (ng - nc) / (ng + nc);
                     }
 
