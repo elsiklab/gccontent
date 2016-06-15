902c8517cdf335f419e367a6a2132601687931a4
Colin <colin.diesh@gmail.com>
2016-06-15 15:39:28 -0500

    Add registerTrackType so that it can be opened by user

--- js/main.js
+++ js/main.js
@@ -13,6 +13,14 @@ return declare( JBrowsePlugin,
 
         // do anything you need to initialize your plugin here
         console.log( "GCContent plugin starting" );
+        browser.registerTrackType({
+            label: 'GCContentXY',
+            type: 'GCContent/View/Track/GCContentXY'
+        });
+        browser.registerTrackType({
+            label: 'GCContentDensity',
+            type: 'GCContent/View/Track/GCContent'
+        });
 
     }
 });
